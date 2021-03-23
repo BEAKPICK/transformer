b67cc7ad1534629e79d8dd6efb89356f4d39624c
beakpick <seokwon2010@gmail.com>
2021-03-24 03:55:46 +0900

    wmt14 dataset

--- transformer_pytorch.py
+++ transformer_pytorch.py
@@ -5,3 +5,21 @@ let's start transformer!
 3. make transformer model
 4. train and evaluate model
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+from torchtext import data
+from torchtext import datasets
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+comments here
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+SRC = data.Field(lower=True,
+                 include_lengths=True,
+                 batch_first=True)
+
+TRG = data.Field(sequential=False)
+
+train, valid, test = datasets.WMT14.splits(exts=('.en','.de'),
+                                           fields=(SRC, TRG),
+                                           root='.data',
+                                           train= 'train.tok.clean.bpe.32000',
+                                           validation= 'newstest2013.tok.bpe.32000',
+                                           test= 'newstest2014.tok.bpe.32000')
