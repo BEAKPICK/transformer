26e8228f41eaee54a1540ecd9c006e6e2d2dc788
Seokwon Lee <seokwon2010@gmail.com>
2021-03-23 22:03:15 +0900

    seperate transformer files

---
+++ transformer_pytorch.py
@@ -0,0 +1,7 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+let's start transformer!
+1. dataset from wmt 2014 English-German or newstest2013 for dev
+2. tokenize them
+3. make transformer model
+4. train and evaluate model
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
